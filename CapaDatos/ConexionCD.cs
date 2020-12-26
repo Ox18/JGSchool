b3176fc054ed4725f5b61d1545f2eb242421f6c3
Alex <73305665+alexwathever@users.noreply.github.com>
2020-12-26 16:47:20 -0500

    Make conection to bd school! implemented entity data!

---
+++ CapaDatos/ConexionCD.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace CapaDatos
+{
+    public class ConexionCD
+    {
+        public static SqlConnection conectarBD()
+        {
+            // Instanciar Clase que facilita la insercion de datos de conexion
+            SqlConnectionStringBuilder generadorCadena = new SqlConnectionStringBuilder();
+
+            // Complementar las propiedades
+
+            // Punto de direccion
+            generadorCadena.DataSource = @"DESKTOP-BKHUJQ5\SQLEXPRESS";
+
+            // Nombre de base de datos
+            generadorCadena.InitialCatalog = "JGSchool";
+
+            // Autenticacion de windows
+            generadorCadena.IntegratedSecurity = true;
+
+            // Recoger la cadena de conexion
+            string cadenaConexion = generadorCadena.ConnectionString;
+
+            // Instanciar conexion a base de datos
+            SqlConnection sqlConnection = new SqlConnection(cadenaConexion);
+
+            // Retornar conexion
+            return sqlConnection;
+        }
+    }
+}
